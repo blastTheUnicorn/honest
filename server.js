875d9435c32a4738ab2488d05b482d9c90938580
Atlante <atlanteavila@gmail.com>
2016-08-23 11:41:02 -0700

    Made a continuing work on front end and merging for latest commit.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose')
 var db = require('./db.js');
 
 
