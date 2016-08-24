cd952d28bd79aa14dbefedf72549e32963fdfd2b
Luisa <lufeza95@gmail.com>
2016-08-23 19:55:28 -0700

    comment test

--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -6,27 +6,27 @@ var should = require('chai').should();
 
 var chaiHttp = require('chai-http');
 
-var server = require('../server');
+var server = require('../server/server');
 
 chai.use(chaiHttp);
 
 describe('server', function(){
-  it('it should have a status code of 200', function(done){
-    chai.request(server)
-        .get('/test')
-        .end(function(err, res){
-          res.should.have.status(200);
-          done();
-        })
-  })
-  it('it should have a res.body property', function(done){
-    chai.request(server)
-        .get('/test')
-        .end(function(err, res){
-          res.should.have.a.property('body');
-          done();
-        })
-  })
+  // it('it should have a status code of 200', function(done){
+  //   chai.request(server)
+  //       .get('/test')
+  //       .end(function(err, res){
+  //         res.should.have.status(200);
+  //         done();
+  //       })
+  // })
+  // it('it should have a res.body property', function(done){
+  //   chai.request(server)
+  //       .get('/test')
+  //       .end(function(err, res){
+  //         res.should.have.a.property('body');
+  //         done();
+  //       })
+  // })
 })
 
 
