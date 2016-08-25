cdb33cbf532603f89092d999f7d92b0cc95d36e6
Garland Riley Jr <garlandrileyjr@gmail.com>
2016-08-25 15:51:23 -0700

    signup auth working

--- server/routes.js
+++ server/routes.js
@@ -8,20 +8,7 @@ var auth = jwt({secret: 'MEOW', userProperty: 'payload'})
 
 var User = mongoose.model('User');
 
-router.post('/signup', function(req, res, next){
-  if(!req.body.username || !req.body.password || !req.body.name || !req.body.email){
-    return res.status(400).json({message: 'Please fill out all fields'});
- };
- var user = new User();
- user.local.name = req.body.name;
- user.local.email = req.body.email;
- user.local.username = req.body.username;
- user.local.password = user.generateHash(req.body.password);
- user.save(function(err){
-  if(err){return next(err);}
-  res.json({token: user.generateJWT()})
- })
-});
+
 
 router.post('/api/login', function(req, res ,next){
   if(!req.body.username || !req.body.password){
