0e7c9e4a28bb55ac5818ad84af1e70e91a963369
Luisa <lufeza95@gmail.com>
2016-08-31 16:02:47 -0700

    logout button

--- app/templates/form/LostFoundController.js
+++ app/templates/form/LostFoundController.js
@@ -1,17 +1,17 @@
 angular.module('LostFoundController', []).controller('LostFoundCtrl', function($timeout, $scope, $location, $http, Token){
 
-      $scope.items =  $scope.items  || [
-        { id: 1, name: 'Cellphone' },
-        { id: 2, name: 'ID/Personal ID Cards' },
-        { id: 3, name: 'Cash' },
-        { id: 4, name: 'Debit/Credit Cards' },
-        { id: 5, name: 'Bags/Purses' },
-        { id: 6, name: 'Books' },
-        { id: 7, name: 'Luggage/Totes' },
-        { id: 8, name: 'Clothing/Hats/Gloves' },
-        { id: 9, name: 'Documents' },
-        { id: 10, name: 'Glasses' },
-        { id: 11, name: 'Other' }
+      $scope.items = [
+        {name: 'Cellphone' },
+        {name: 'ID/Personal ID Cards' },
+        {name: 'Cash' },
+        {name: 'Debit/Credit Cards' },
+        {name: 'Bags/Purses' },
+        {name: 'Books' },
+        {name: 'Luggage/Totes' },
+        {name: 'Clothing/Hats/Gloves' },
+        {name: 'Documents' },
+        {name: 'Glasses' },
+        {name: 'Other' }
       ];
 
 })
