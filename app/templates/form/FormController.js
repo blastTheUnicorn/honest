5fa874dcf290bd1ea28f8a065527afed93e3a473
Luisa <lufeza95@gmail.com>
2016-08-31 20:05:46 -0700

    worked on the feed view and on the get request

--- app/templates/form/FormController.js
+++ app/templates/form/FormController.js
@@ -1,5 +1,5 @@
 angular.module('FormController', [])
-.controller('FormCtrl', function($scope, $http, Token){
+.controller('FormCtrl', function($scope, $http, Token, $location){
   
   // store all form data in this object
   $scope.formData = {
@@ -11,7 +11,9 @@ angular.module('FormController', [])
     var userID = Token.currentUser()
     return $http.post('/api/user/' + userID, $scope.formData).success(function(data){
       console.log("Testing", data);
+      $location.path('/form/location');
+      $scope.formData = {};
     })
-  }
+  };
 
 });
