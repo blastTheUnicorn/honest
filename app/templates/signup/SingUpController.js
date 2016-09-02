4faf73858820678f23132a20d0777c4404d54bde
Luisa <lufeza95@gmail.com>
2016-09-02 12:33:54 -0700

    implement valitadion and add toast to signup view

--- app/templates/signup/SingUpController.js
+++ app/templates/signup/SingUpController.js
@@ -1,5 +1,5 @@
 angular.module('SingUpController', [])
-.controller('SignUpCtrl', function($scope, $location, $http, Token){
+.controller('SignUpCtrl', function($scope, $location, $http, Token, $mdToast){
   $scope.user = {};
 
   $scope.signUp = function(){
@@ -8,11 +8,19 @@ angular.module('SingUpController', [])
       url: '/api/signUp',
       contentType: 'application/json',
       data: $scope.user
-    }).then(function(res){
+    }).then(function successCallback(res){
       Token.saveToken(res.data.token)
       $location.path('/form/location')
       return res;
+    }, function errorCallback(res){
+      $mdToast.show(
+        $mdToast.simple()
+        .textContent('User or Email already exists')
+        .capsule(true)
+        .hideDelay(3000) 
+        .position('right')
+      )
     })
-  }
+  };
 
 })
